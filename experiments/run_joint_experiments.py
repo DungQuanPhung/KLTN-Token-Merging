029f334b5d01b7e67dec2fd384380e26664f5774
Nguyen Tuyen <hotuyenpt@gmail.com>
2026-06-14 00:07:21 +0700

    update

--- experiments/run_joint_experiments.py
+++ experiments/run_joint_experiments.py
@@ -81,7 +81,7 @@ SUPPLEMENT_FILES: List[str] = [
 # Change MODEL_TYPE to switch between encoders.  Add new entries to
 # _MODEL_CONFIGS to register additional pretrained checkpoints.
 
-MODEL_TYPE = "bert"   # "bert" | "t5"
+MODEL_TYPE = "t5"   # "bert" | "t5"
 
 _MODEL_CONFIGS = {
     "bert": "bert-base-uncased",
